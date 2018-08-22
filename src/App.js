cfac868befe0c6355474cdc627eb5e9165329e8e
yogiraj99 <ytambake@gmail.com>
2018-08-22 16:37:36 +0530

    Added functionality of stepWise execution.

--- src/App.js
+++ src/App.js
@@ -17,15 +17,15 @@ class App extends Component {
       registerTable: [],
       message: INITIALMESSAGE,
       prints: [],
-      stack: []
+      stack: [],
+      isExecutingStepWise: false
     };
     this.executeCode = this.executeCode.bind(this);
+    this.executeStepWise = this.executeStepWise.bind(this);
+    this.updateRegisterAndStack = this.updateRegisterAndStack.bind(this);
+    this.executeNextLine = this.executeNextLine.bind(this);
     this.handleCodeEdit = this.handleCodeEdit.bind(this);
     this.setHasChangedPropertyForChangedRows = this.setHasChangedPropertyForChangedRows.bind(this);
-  }
-
-  handleCodeEdit(editor) {
-    this.setState({editor})
   }
 
   render() {
@@ -35,7 +35,9 @@ class App extends Component {
             <EditorComp initailCode={INITIALCODE} onEdit={this.handleCodeEdit}/>
             <MessageBox message={this.state.message}/>
             <div>
-              <button onClick={this.executeCode}>Execute</button>
+              <button onClick={this.executeStepWise} disabled={this.state.isExecutingStepWise}>Step Into</button>
+              <button onClick={this.executeCode}>Run</button>
+              <button onClick={this.executeNextLine} disabled={!this.state.isExecutingStepWise}>Next</button>
             </div>
           </div>
           <div className="outputSection">
@@ -48,6 +50,12 @@ class App extends Component {
           </div>
         </div>
     );
+  }
+
+  handleCodeEdit(editor) {
+    this.setState({editor});
+    this.clearTableAndStack();
+    this.setAsNotExecutingStepWise();
   }
 
   setHasChangedPropertyForChangedRows(event) {
@@ -77,11 +85,55 @@ class App extends Component {
     try {
       machine.load(numberedCode);
       machine.execute();
-      this.setState({registerTable: machine.getTable(), message: INITIALMESSAGE});
-      this.setState({prints: machine.getPrn(), stack: machine.getStack()});
+      this.setAsNotExecutingStepWise();
+      this.setState({
+        registerTable: machine.getTable(),
+        prints: machine.getPrn(),
+        stack: machine.getStack(),
+        message: INITIALMESSAGE
+      });
     } catch (e) {
       this.setState({message: e, registerTable: []})
     }
+  }
+
+  executeStepWise() {
+    let lines = this.state.editor.split(/\n/);
+    let numberedCode = lines.map((l, i) => `${(i + 1) * 10} ${l.trim()}`).join("\n");
+    let machine = this.state.machine;
+    try {
+      machine.load(numberedCode);
+      machine.executeStepWise(this.updateRegisterAndStack);
+      this.clearTableAndStack();
+      this.setAsExecutingStepWise();
+    } catch (e) {
+      this.setState({message: e, registerTable: []})
+    }
+  }
+
+  clearTableAndStack() {
+    this.setState({registerTable: [], prints: [], stack: []})
+  }
+
+  executeNextLine() {
+    this.state.machine.nextStep();
+  }
+
+  setAsNotExecutingStepWise() {
+    this.setState({isExecutingStepWise: false})
+  }
+
+  setAsExecutingStepWise() {
+    this.setState({isExecutingStepWise: true})
+  }
+
+  updateRegisterAndStack(state) {
+    let {A, B, C, D, EQ, NE, GT, LT, CL, NL, SL, PRN, INST, STK} = state;
+    let registerTable = this.state.registerTable;
+    let prints = this.state.prints;
+    registerTable.push({A, B, C, D, EQ, NE, GT, LT, CL, NL, PRN, INST});
+    prints.push(PRN);
+    this.setState({registerTable, prints, stack: STK});
   }
 }
 
